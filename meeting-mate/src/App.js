714ceab6000af9b929ee0ace5bca44baf80551f0
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2024-01-06 14:38:37 +0530

    Update App.js

--- meeting-mate/src/App.js
+++ meeting-mate/src/App.js
@@ -14,15 +14,3 @@ function App() {
 }
 
 export default App;
-
-
-//Client ID:  584206551589-1m6q9qtm83kftpleeo5g8g9tupvcb84d.apps.googleusercontent.com
-// Client Secret: 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
-
-
-
-// Calender
-// API Key: AIzaSyBG1HkyvKdu4ocSljB8-9iuh9nlSAhVy4w
-
-
-// Working with google authentication: https://www.youtube.com/watch?v=UUJfTsn6S_Y
